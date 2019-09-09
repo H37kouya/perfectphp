e6ad72873dea7369f15de4bc0d395bf13028c2e8
H37kouya <kouttc.84171@gmail.com>
2019-09-09 10:51:09 +0900

    ナビゲーションの追加

--- Views/layout.php
+++ Views/layout.php
@@ -12,6 +12,18 @@
         <h1><a href="<?php echo $base_url; ?>">Mini Blog</a></h1>
     </header>
 
+    <nav id="nav" class="nav">
+        <p>
+            <?php if ($session->isAuthenticated()): ?>
+            <a href="<?php echo $base_url; ?>/">ホーム</a>
+            <a href="<?php echo $base_url; ?>/account">アカウント</a>
+            <?php else: ?>
+            <a href="<?php echo $base_url; ?>/account/signin">ログイン</a>
+            <a href="<?php echo $base_url; ?>/account/signup">アカウント登録</a>
+            <?php endif; ?>
+        </p>
+    </nav>
+
     <main id="main" class="main">
         <?php echo $_content; ?>
     </main>
