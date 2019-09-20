dce1e344a45c4ebff85352e7ebbb421f1c79ec0a
H37kouya <h37kouya@gmail.com>
2019-09-20 23:59:29 +0900

    [add]: ログイン画面にデザインを追加

--- views/account/signin.php
+++ views/account/signin.php
@@ -1,17 +1,16 @@
 <?php $this->setLayoutVar('title', 'ログイン'); ?>
 
 <section>
-    <h2>ログイン</h2>
+    <h2 class="uk-heading-bullet">ログイン</h2>
 
-    <p><a href="<?php echo asset('/account/signup');?>">新規ユーザー登録</a>
+    <p><a href="<?php echo asset('/account/signup'); ?>" class="uk-button uk-button-secondary">新規ユーザー登録</a>
     </p>
 
-    <form action="<?php echo asset('/account/authenticate'); ?>"
-        method="post">
+    <form action="<?php echo asset('/account/authenticate'); ?>" method="post">
         <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>">
 
-        <?php if (isset($errors) && count($errors) > 0): ?>
-        <?php echo $this->render('errors', ['errors' => $errors]); ?>
+        <?php if (isset($errors) && count($errors) > 0) : ?>
+            <?php echo $this->render('errors', ['errors' => $errors]); ?>
         <?php endif; ?>
 
         <?php echo $this->render('account/inputs', [
@@ -20,7 +19,7 @@
         ]); ?>
 
         <p>
-            <input type="submit" value="ログイン">
+            <input type="submit" value="ログイン" class="uk-button uk-button-primary">
         </p>
     </form>
 </section>
