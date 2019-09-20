fcef7a7e46dd3ed02d97c35d5e47322d6961bf34
H37kouya <h37kouya@gmail.com>
2019-09-20 23:58:15 +0900

    [add]: inputsにデザインを追加

--- views/account/inputs.php
+++ views/account/inputs.php
@@ -1,15 +1,15 @@
-<table>
+<table class="uk-table uk-width-large">
     <tbody>
         <tr>
             <th>ユーザーID</th>
             <td>
-                <input type="text" name="user_name" value="<?php echo $this->escape($user_name); ?>">
+                <input type="text" name="user_name" value="<?php echo $this->escape($user_name); ?>" class="uk-input">
             </td>
         </tr>
         <tr>
             <th>パスワード</th>
             <td>
-                <input type="password" name="password" value="<?php echo $this->escape($password); ?>">
+                <input type="password" name="password" value="<?php echo $this->escape($password); ?>" class="uk-input">
             </td>
         </tr>
     </tbody>
