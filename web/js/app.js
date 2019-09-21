9ac10b838f4fc245505f0fe39b5badb6d3a4bd42
H37kouya <h37kouya@gmail.com>
2019-09-21 10:24:03 +0900

    [change]: ボタンの色(緑)を追加

--- web/js/app.js
+++ web/js/app.js
@@ -115,8 +115,8 @@
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\khasd\code\perfectphp\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\khasd\code\perfectphp\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/h37kouya/var/www/html/perfectphp/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/h37kouya/var/www/html/perfectphp/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
