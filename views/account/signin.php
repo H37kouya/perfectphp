6517712a0ae5426c643313aee478f0e627d24792
H37kouya <h37kouya@gmail.com>
2019-09-21 10:24:29 +0900

    [change]: 新規登録の場所とデザインを変更

--- views/account/signin.php
+++ views/account/signin.php
@@ -3,8 +3,7 @@
 <section>
     <h2 class="uk-heading-bullet">ログイン</h2>
 
-    <p><a href="<?php echo asset('/account/signup'); ?>" class="uk-button uk-button-secondary">新規ユーザー登録</a>
-    </p>
+
 
     <form action="<?php echo asset('/account/authenticate'); ?>" method="post">
         <input type="hidden" name="_token" value="<?php echo $this->escape($_token); ?>">
@@ -20,6 +19,7 @@
 
         <p>
             <input type="submit" value="ログイン" class="uk-button uk-button-primary">
+            <a href="<?php echo asset('/account/signup'); ?>" class="uk-button uk-button-success">新規ユーザー登録</a>
         </p>
     </form>
 </section>
